ea7d8083e7602b2b75af80d633c1178743cc598c
trmyint97 <trmyint97@gmail.com>
2022-04-08 19:39:48 -0400

    Removed colored labels for graph

--- PCA.py
+++ PCA.py
@@ -6,7 +6,7 @@ def PCA_analysis(X):
     pca.fit(X)
     return pca.transform(X)
 
-def graph_PCA(X, y):
+def graph_PCA(X):
     X_transformed = PCA_analysis(X)
-    plt.scatter(X_transformed[:, 0], X_transformed[:, 1], c=y)
+    plt.scatter(X_transformed[:, 0], X_transformed[:, 1])
     plt.show()
